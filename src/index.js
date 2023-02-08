812d1762eeafd9a33f111b46524d1accbb1a48ca
MissisVolk <113783910+MissisVolk@users.noreply.github.com>
2023-02-09 00:24:39 +0300

    docs: new file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+module.exports = function towelSort (matrix) {
+  let arr = [];
+
+  if(matrix == undefined){
+    return arr;
+  }
+
+  for(let i = 0; i < matrix.length; i++) {
+    if((i%2) === 0) {
+      for(let j = 0; j < matrix[i].length; j++) {
+        arr.push(matrix[i][j]);
+      }
+    } else {
+      for(let k = (matrix[i].length - 1); k > -1; k--) {
+        arr.push(matrix[i][k]);
+      }
+    }
+  }
+
+  return arr;
+};
